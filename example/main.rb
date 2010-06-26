5eed17690aa9c2248f36cc51fddd2561b132f50b
Rawane ZOSSOU <dev@raw1z.fr>
2010-06-26 03:04:19 +0200

    translate the example's comments and clean the example's directory

--- example/main.rb
+++ example/main.rb
@@ -2,37 +2,34 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'tesler'
 
-# on supprime le repertoire de destination
+# delete destination directory
 FileUtils.rm_r("dest") if File.exists?("dest")
 
 directory 'dest' do
 
-  # copie de fichiers dans le dossier de destination
+  # copy files into the dest dir
   copy 'src\file1'
   copy 'src\file2'
   copy 'src\subdir1\file4'
 
-  # creation d'un sous-dossier dans le dossier de destination
+  # create a sub-directory and copy files into it
   directory 'subdir1' do
     copy 'src\file3'
     copy 'src\subdir1\file5'
-
-    # copie avec un chemin absolu
-    copy 'c:\temp'
     
-    # sous-dossier dans le sous-dossier
+    # a sub-directory in the sub-directory
     directory 'subdir3' do
       copy 'src\file8'
 
-      # copie d'un fichier en le renommant
+      # copy a file and rename it
       copy 'src\file9', :rename => 'renamed'
     end
   end
 
-  # copie d'un dossier entier
+  # copy an entire directory
   copy 'src\subdir2'
 
-  # copie d'un dossier entier en le renommant
+  # copy an entire directory and rename it
   copy 'src\subdir2', :rename => 'renamed'
 
 end
